0193a8c5ff30ba658521a43c2704ce25e33ae308
linzlb <798648334@qq.com>
2021-04-20 11:56:30 +0800

    SpringDemo#Redission资料

--- src/main/java/com/linzlb/controller/RedisController.java
+++ src/main/java/com/linzlb/controller/RedisController.java
@@ -7,6 +7,7 @@ import com.linzlb.utils.RedissionUtils;
 import com.linzlb.utils.ResponseResultUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +15,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.Resource;
 import java.io.IOException;
 
 /**
@@ -68,15 +67,16 @@ public class RedisController {
 
 
     @ApiOperation(value = "测试redisLock", notes = "测试redisLock")
-    @RequestMapping(value = "/redisLock" , method= {RequestMethod.GET} )
+    @RequestMapping(value = "/redission" , method= {RequestMethod.GET} )
     public void redisLock() throws IOException {
 
-        logger.info(host+port+password+database);
+        //logger.info(host+port+password+database);
         Config config = new Config();
-        String url = "redis://" + host
-				+ ":" + port;
+        String url = "redis://" + host + ":" + port;
 		config.useSingleServer().setAddress(url);
         RedissonClient redission = redissionUtils.getInstance().getRedisson(config);
+
+        //testRedisLock
         for (int i = 0;i < 50;i++) {
             new Thread(() -> {
                 redissionLockUtil.lock(redission, "testLock");
@@ -84,12 +84,32 @@ public class RedisController {
         }
 
 
-        // 最常见的使用方法
-//        RLock lock = redission.getLock("anyLock");
+        // Redisson的分布式可重入锁RLock 最常见的使用方法
+        RLock lock = redission.getLock("anyLock");
 //        lock.lock();
-//        logger.info("lock");
 //        lock.unlock();
-//        logger.info("unlock");
+        //另外Redisson还通过加锁的方法提供了leaseTime的参数来指定加锁的时间。超过这个时间后锁便自动解开了。
+        // 加锁以后10秒钟自动解锁
+        // 无需调用unlock方法手动解锁
+//        lock.lock(10, TimeUnit.SECONDS);
+        // 尝试加锁，最多等待100秒，上锁以后10秒自动解锁
+//        boolean res = lock.tryLock(100, 10, TimeUnit.SECONDS);
+//        if (res) {
+//            try {
+//
+//            } finally {
+//                lock.unlock();
+//            }
+//        }
+        //大家都知道，如果负责储存这个分布式锁的Redisson节点宕机以后，而且这个锁正好处于锁住的状态时，这个锁会出现锁死的状态。为了避免这种情况的发生，Redisson内部提供了一个监控锁的看门狗，它的作用是在Redisson实例被关闭前，不断的延长锁的有效期。默认情况下，看门狗的检查锁的超时时间是30秒钟，也可以通过修改Config.lockWatchdogTimeout来另行指定。
+//        Redisson同时还为分布式锁提供了异步执行的相关方法：
+//        RLock lock = redisson.getLock("anyLock");
+//        lock.lockAsync();
+//        lock.lockAsync(10, TimeUnit.SECONDS);
+//        Future<Boolean> res = lock.tryLockAsync(100, 10, TimeUnit.SECONDS);
+
+//https://blog.csdn.net/liuxiao723846/article/details/88131065?utm_term=redission%E5%88%86%E5%B8%83%E5%BC%8F%E9%94%81&utm_medium=distribute.pc_aggpage_search_result.none-task-blog-2~all~sobaiduweb~default-2-88131065&spm=3001.4430
+
     }
 
 
